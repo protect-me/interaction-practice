af4a89e8d79a27f055a473c2b725606829526689
protect-me <pmfwiww@gmail.com>
2021-06-06 14:47:13 +0900

    create project

---
+++ app.js
@@ -0,0 +1,35 @@
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h1");
+//END SECTION
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
+
+//SCROLLMAGIC
+const controller = new ScrollMagic.Controller();
+
+// Scenes
+const scene = new ScrollMagic.Scene({
+  duration: 9000,
+  triggerElement: intro,
+  triggerHook: 0,
+})
+  .addIndicators()
+  .setPin(intro)
+  .addTo(controller);
+
+// Video Animation
+let accelamount = 0.05;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on("update", (e) => {
+  scrollpos = e.scrollPos / 1000;
+  // console.log(e);
+});
+
+setInterval(() => {
+  delay += (scrollpos - delay) * accelamount;
+  // console.log(scrollpos, delay);
+  video.currentTime = delay;
+}, 33.3);
